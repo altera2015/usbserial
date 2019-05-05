76369ce07326e12dced8b06333c64a613725f116
Ron Bessems <rbessems@gmail.com>
2019-05-05 12:24:29 -0400

    Added missing mocked UsbPort called EchoPort

---
+++ test/echo_port.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+import 'dart:typed_data';
+import 'types.dart';
+
+/// Test harnass stub for UsbPort.
+/// Sends the data back with a delay.
+class EchoPort implements AsyncDataSinkSource {
+  Stream<Uint8List> _stream;
+  StreamController<Uint8List> _controller;
+  bool _running = false;
+  List<int> _buffer;
+  final Duration writeDelay;
+
+  Stream<Uint8List> fakeData() {
+    void start() {
+      _running = true;
+      if (_buffer.length > 0) {
+        _controller.add(Uint8List.fromList(_buffer));
+        _buffer.clear();
+      }
+    }
+
+    void stop() {
+      _running = false;
+    }
+
+    _controller = StreamController<Uint8List>(
+        onListen: start, onPause: stop, onResume: start, onCancel: stop);
+
+    return _controller.stream;
+  }
+
+  EchoPort({this.writeDelay = const Duration(seconds: 0)}) {
+    _stream = fakeData();
+    _buffer = [];
+  }
+
+  Stream<Uint8List> get inputStream {
+    return _stream;
+  }
+
+  Future<void> write(Uint8List data) async {
+    Future<void>.delayed(writeDelay, () {
+      if (_running) {
+        _controller.add(data);
+      } else {
+        _buffer.addAll(data);
+      }
+    });
+  }
+}
