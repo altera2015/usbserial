b530f3e9c84b7c5d1f86caf534562fcf0ccf5057
Ron Bessems <rbessems@gmail.com>
2019-05-07 15:45:42 -0400

    Added test coverage on the Transformers and Transactions.

--- test/echo_port.dart
+++ test/echo_port.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:typed_data';
 import '../lib/types.dart';
-import '../lib/transaction.dart';
 
 /// Test harnass stub for UsbPort.
 /// Sends the data back with a delay.
@@ -9,14 +8,16 @@ class EchoPort extends AsyncDataSinkSource {
   Stream<Uint8List> _stream;
   StreamController<Uint8List> _controller;
   bool _running = false;
-  List<int> _buffer;
+  List<Uint8List> _buffer;
   final Duration writeDelay;
 
   Stream<Uint8List> fakeData() {
     void start() {
       _running = true;
       if (_buffer.length > 0) {
-        _controller.add(Uint8List.fromList(_buffer));
+        _buffer.forEach((data) {
+          _controller.add(data);
+        });
         _buffer.clear();
       }
     }
@@ -40,13 +41,25 @@ class EchoPort extends AsyncDataSinkSource {
     return _stream;
   }
 
+  Future<void> _write(Uint8List data) async {
+    if (_running) {
+      _controller.add(data);
+    } else {
+      _buffer.add(data);
+    }
+  }
+
   Future<void> write(Uint8List data) async {
-    Future<void>.delayed(writeDelay, () {
-      if (_running) {
-        _controller.add(data);
-      } else {
-        _buffer.addAll(data);
-      }
-    });
+    if ( writeDelay == Duration(seconds: 0)) {
+      return _write(data);
+    } else {
+      Future<void>.delayed(writeDelay, () {
+        return _write(data);
+      });
+    }
+  }
+
+  void close() {
+    _controller.close();
   }
 }
