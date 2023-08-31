ec41cac03fd2e50523f0c61592247bb6c35a4762
eparisot <parisot.eric.mail@gmail.com>
2023-08-31 17:08:51 +0200

    fix arg optionals

--- lib/transaction.dart
+++ lib/transaction.dart
@@ -41,7 +41,7 @@ class Transaction<T> {
   /// var c = Transaction.terminated(p.inputStream, Uint8List.fromList([13, 10]));
   /// ```
   static Transaction<Uint8List> terminated(Stream<Uint8List> stream,
-      Uint8List terminator, int maxLen = 1024, bool stripTerminator = true) {
+      Uint8List terminator, int maxLen: 1024, bool stripTerminator: true) {
     return Transaction<Uint8List>(
         stream,
         TerminatedTransformer.broadcast(
